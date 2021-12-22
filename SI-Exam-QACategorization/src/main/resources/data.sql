e7d92c5045525d14c063ab4d3349bfb818a66ee4
Tureczek <n.m.tureczek@gmail.com>
2021-12-22 12:19:34 +0100

    Updated database with questions and answers

--- SI-Exam-QACategorization/src/main/resources/data.sql
+++ SI-Exam-QACategorization/src/main/resources/data.sql
@@ -1,6 +1,6 @@
-INSERT INTO answers (id, question, answer) VALUES (0, 'Eric Cartman', 'cartman@southpark.us');
-INSERT INTO answers (id, question, answer) VALUES (1, 'Kenny McCormick', 'kenny@southpark.us');
-INSERT INTO answers (id, question, answer) VALUES (2, 'login', 'login@southpark.us');
-INSERT INTO answers (id, question, answer) VALUES (3, 'cant login', 'cant@southpark.us');
-INSERT INTO answers (id, question, answer) VALUES (4, 'Jerome "Chef" McElroy', 'chef@southpark.us');
-INSERT INTO answers (id, question, answer) VALUES (5, 'why do i need a login', 'why@southpark.us');
+INSERT INTO answers (id, question, answer) VALUES (0, 'Retrieve password', 'At the login page, there is an option to press "Forgot password"');
+INSERT INTO answers (id, question, answer) VALUES (1, 'Cant find game', 'The game you are looking for, might not exist yet. But dont fear, the game might come soon');
+INSERT INTO answers (id, question, answer) VALUES (2, 'login problems', 'Have you entered the right password?');
+INSERT INTO answers (id, question, answer) VALUES (3, 'cant login', 'Are you created as a user');
+INSERT INTO answers (id, question, answer) VALUES (4, 'Can i apply for a job?', 'Sure you can. If you have interest in joining our team send us a mail at job@gamehub.com');
+INSERT INTO answers (id, question, answer) VALUES (5, 'why do i need a login', 'You dont, you can browse all our games without logging in. but you will get many more functions if you login, such as creating review and state your opinion on the latest games.');
